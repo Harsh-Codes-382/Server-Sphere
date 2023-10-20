d57f4683a8143a052efa2996c934d44624a1236f
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-10-20 15:06:57 +0530

    Set the UploadThing for public route

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default authMiddleware({
+  // We add the route which is public like uploadYour Server Image
+  publicRoutes: ["/api/uploadthing"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
