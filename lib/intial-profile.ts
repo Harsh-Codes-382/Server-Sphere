074b36b121c0cebdb6e39f56ad98ed95146be2fc
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-10-20 15:11:06 +0530

    It create a profile if user not loggged in or just redirect to server he joined

---
+++ lib/intial-profile.ts
@@ -0,0 +1,33 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+  // See if user previouly Logged in or it is first time
+  const user = await currentUser();
+
+  if (!user) {
+    //If user didn't logged in then redirect to login in
+    return redirectToSignIn();
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+  if (profile) {
+    return profile;
+  }
+
+  //   If profile Not existed in database then create new one
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
+};
