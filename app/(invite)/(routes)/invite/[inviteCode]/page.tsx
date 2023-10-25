b0c3c215b72beb4578398756ab37513287317a09
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-10-25 19:04:34 +0530

    This is a page opens up when user paste the invitaion Link from invite-modal and then this page will appear and it also checks if user already member of this server then redirect to this server else invite and add the user as a member of this server

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,65 @@
+import { Currentprofile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface InviteCodeProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+const InviteCodePage = async ({ params }: InviteCodeProps) => {
+  const profile = await Currentprofile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  // If there is not a inviteCode in  url then return to homePage
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  // Now check if the user who is trying to get into server using "inviteCode" if already member of this server
+  const User_Already_Exist_In_Server = await db.server.findFirst({
+    where: {
+      // If you got to find the server with this invitecode
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          // And you also found the user id as a member of that server
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (User_Already_Exist_In_Server) {
+    // If user already a member of the server then just redirect to that server
+    redirect(`/servers/${User_Already_Exist_In_Server.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            // By default user role will be GUEST so no need to set that
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+  return (
+    <>
+      <div className="">Hello</div>
+    </>
+  );
+};
+
+export default InviteCodePage;
