0136e2d3e7751e48f22faf6b71352b7162a98ca4
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-10-25 18:28:56 +0530

    This is a Modal store which have type & data & open & close  function for every Modal to open.It have onOpen() which sets the isOpen true & set the data & type which we sent here from button where we want to open a modal of particular type  and onClose() stes isOpen to false

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,29 @@
+// This is hook modal store from where we will handle all modals.
+
+import { Server } from "@prisma/client";
+import { create } from "zustand";
+
+export type ModalType = "createServer" | "invite";
+
+interface ModalData {
+  server?: Server;
+}
+
+interface ModalStore {
+  type: ModalType | null;
+  isOpen: boolean;
+  data: ModalData;
+  // Tells Which kind of modal we want to open
+  onOpen: (type: ModalType, data?: ModalData) => void;
+  onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+  isOpen: false,
+  data: {},
+  // onOpen set the isOpen to true and type of modal to type we sent
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  // onOpen set the isOpen to false and type of modal null.
+  onClose: () => set({ isOpen: false, type: null }),
+}));
