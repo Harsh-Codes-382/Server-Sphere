40ce8d1ed27e9cc7df923f8c5607a5944bdca33f
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-11-07 15:52:54 +0530

    This is a route for kicking out the member and changing the role of memeber and memberId sent here through params

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -2,6 +2,63 @@ import { Currentprofile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function DELETE(req:Request, {params}: {params: {memberId: string}}){
+  try {
+    const profile = await Currentprofile();
+
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+      if (!profile) {
+        return new NextResponse("Unauthorized", { status: 401 });
+      }
+
+      if (!params.memberId) {
+        return new NextResponse("MemberId Not Present", { status: 400 });
+      }
+
+      if (!serverId) {
+        return new NextResponse("serverId Not Present", { status: 400 });
+      }
+
+      const server = await db.server.update({
+        where:{
+          id: serverId,
+          profileId:profile.id,
+        },
+        data:{
+          members:{
+            // Delete the member whose memberId is sent here
+            deleteMany:{
+              id:params.memberId,
+              // And Admin can't kick out himself
+              profileId: {
+                not: profile.id
+              }
+            }
+          }
+        },
+        include:{
+          members:{
+            include:{
+              profile: true,
+            },
+            orderBy:{
+              role:"asc",
+            }
+          }
+        }
+      });
+
+      return NextResponse.json(server);
+    
+  } catch (error) {
+    return new NextResponse("Deletion of Member goes wrong", {status: 500});
+  }
+}
+
+
 export async function PATCH( req:Request, {params}: {params: {memberId: string}}){
 
     try{
