62d920e490880877d1d2d64263657f63e9f1a671
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-11-07 16:56:38 +0530

    Now added the delete function to delete this server with serveId we sent here from delete-server-modal

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,8 @@ import { Currentprofile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+
+// This function is for updating the server
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string } }
@@ -35,6 +37,44 @@ export async function PATCH(
   } catch (error) {
     console.log(error);
 
-    return new NextResponse("Internal Error", { status: 500 });
+    return new NextResponse(" Updating server Internal Error", { status: 500 });
+  }
+}
+
+
+
+
+
+// This function is for deleting the server
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await Currentprofile();
+
+    if (!profile) {
+      return new NextResponse("UnAuthorized", { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Missing ServerId in deleting Server", {
+        status: 400,
+      });
+    }
+
+    const server = await db.server.delete({
+      // Delete the server who's id is sent here through params
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log(error);
+
+    return new NextResponse("Deleting Server Internal Error", { status: 500 });
   }
 }
