04ee7d10b0a447bb86de1bbed7ce0e14bb041ee2
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-11-09 12:35:34 +0530

    I added he serverSearch Component here and also made the two objects for icon of both role and channel type and we pass the all server info like it's member, channels with it's type as a props to serverSearch component

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,11 +1,29 @@
 import { Currentprofile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ServerHeader } from "./server-header";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import ServerSearch from "./server-search";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
 
 interface ServerSideBarProps {
   serverId: string;
+}
+
+// For different channel type different icons
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4"/>,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4"/>,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4"/>
+}
+
+// For different role of member different icons
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500"/> ,
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500"/>
+
 }
 
 const ServerSideBar = async ({ serverId }: ServerSideBarProps) => {
@@ -42,7 +60,7 @@ const ServerSideBar = async ({ serverId }: ServerSideBarProps) => {
   //    It is we specify in prisma schema "ChannelType"
 
   //   It is for Text channels
-  const textChannels = server?.channels.filter(
+  const TextChannels = server?.channels.filter(
     (channel) => channel.type === ChannelType.TEXT
   );
 
@@ -72,6 +90,53 @@ const ServerSideBar = async ({ serverId }: ServerSideBarProps) => {
     <>
       <div className="flex flex-col text-primary h-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
         <ServerHeader server={server} role={role} />
+        <ScrollArea className="flex-1 px-3">
+          <div className="mt-2">
+            {/* Sending all the channels & members as a props in serverSearch Component  
+            so we can search all channels in serversearch componenet*/}
+            <ServerSearch
+              data={[
+                {
+                  label: "Text Channels",
+                  type: "channel",
+                  data: TextChannels?.map((channel) => ({
+                    id: channel.id,
+                    name: channel.name,
+                    icon: iconMap[channel.type],
+                  })),
+                },
+                {
+                  label: "Audio Channels",
+                  type: "channel",
+                  data: AudioChannels?.map((channel) => ({
+                    id: channel.id,
+                    name: channel.name,
+                    icon: iconMap[channel.type],
+                  })),
+                },
+                {
+                  label: "Video Channels",
+                  type: "channel",
+                  data: VideoChannels?.map((channel) => ({
+                    id: channel.id,
+                    name: channel.name,
+                    icon: iconMap[channel.type],
+                  })),
+                },
+                {
+                  label: "Members",
+                  type: "member",
+                  data: members?.map((member) => ({
+                    id: member.id,
+                    name: member.profile.name,
+                    // Based on role of member icon will be sent in props
+                    icon: roleIconMap[member.role],
+                  })),
+                },
+              ]}
+            />
+          </div>
+        </ScrollArea>
       </div>
     </>
   );
