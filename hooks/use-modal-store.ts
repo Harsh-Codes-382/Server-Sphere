b107b82a1a33f209af0d771a6ca2ba5c141a7b9f
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-11-10 14:22:59 +0530

    Added the 'channelType' which is optional to pass to a modal it tells in modla that which type of channel we want to open by default like TEXT, VIDEO if we send AUDIO as a channelType then when  create channel modal opens then AUDIO type will be there as a default

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,12 +1,14 @@
 // This is hook modal store from where we will handle all modals.
 
-import { Server } from "@prisma/client";
+import { ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
 export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer";
 
 interface ModalData {
   server?: Server;
+  // It means which type of channel we want to open so send that info as well but it is optional to send 
+  channelType?: ChannelType;
 }
 
 interface ModalStore {
