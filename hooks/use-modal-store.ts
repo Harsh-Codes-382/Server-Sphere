d2bae46686a4bc41dc4ed8940d83debde704354c
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-11-15 19:58:12 +0530

    Added the editChannel type so we can open a editModal on sending the type of editChannel in onOpen()

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -5,7 +5,7 @@ import { create } from "zustand";
 
 
 export type ModalType = "createServer" | "invite" | "editServer" | "members" 
-                        | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel";
+                        | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel";
 
 interface ModalData {
   server?: Server;
