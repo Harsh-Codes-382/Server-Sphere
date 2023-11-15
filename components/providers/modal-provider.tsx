66d5facf1c425a475806838b148c2393d50c1bfd
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-11-15 19:54:46 +0530

    Added the deleteChannel and EditChannel Modal in provider

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,14 +3,16 @@
 
 // This component renders all modals
 
-import { CreateServerModal } from "@/components/Modals/create-server-modal";
 import { useEffect, useState } from "react";
+import { CreateServerModal } from "@/components/Modals/create-server-modal";
 import { InviteModal } from "@/components/Modals/invite-modal";
 import { EditServerModal } from "@/components/Modals/edit-server-modal";
 import { MembersModal } from "@/components/Modals/members-modal";
 import { CreateChannelModal } from "@/components/Modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/Modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/Modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/Modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/Modals/edit-channel-modal";
 
 export const ModalProvider = () => {
   // By doing this here we are preventing the modals renders on server side so, avoid hydration Error
@@ -33,6 +35,8 @@ export const ModalProvider = () => {
       <CreateChannelModal/>
       <LeaveServerModal/>
       <DeleteServerModal/>
+      <DeleteChannelModal/>
+      <EditChannelModal/>
     </>
   );
 };
