ab420db786909740f29ec974bfd656d4be7dc9eb
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-11-18 19:06:00 +0530

    Added the onlclick functionality so on click of any channel we redirected to that channel page and also we fixed the bug which just appears when we added the onclick. Issue was that if we click on trash or edit button the modal was opening but before that we were redirected to channel means that global onclick function overwrites the other onclick so to handle that we made the onAction()

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -6,7 +6,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "@/components/action-tooltip";
 
-import { useModal } from "@/hooks/use-modal-store";
+import { ModalType, useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps{
     channel: Channel;
@@ -29,10 +29,20 @@ const ServerChannel = ({channel, server, role} : ServerChannelProps) => {
     const {onOpen} = useModal();
 
     const Icon = iconMap[channel.type];
+
+    const onclick = ()=>{
+      router.push(`/servers/${params?.serverId}/channels/${channel.id}`)
+    }
+
+    // so If we click on edit or delete button then only that event should execute not other like which opens the channel page
+    const onAction = (e: React.MouseEvent, action:ModalType) =>{
+      e.stopPropagation();
+      onOpen(action, {channel, server});
+    }
   return (
     <>
       <button
-        onClick={() => {}}
+        onClick={onclick}
         className={cn(
           "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
           params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -54,14 +64,14 @@ const ServerChannel = ({channel, server, role} : ServerChannelProps) => {
           <div className="ml-auto flex items-center gap-x-2">
             <ActionTooltip label="Edit">
               <Edit
-                onClick={() => onOpen("editChannel", { server, channel })}
+                onClick={(e) => onAction(e, "editChannel")}
                 className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 ddark:hover:text-zinc-300 transition"
               />
             </ActionTooltip>
 
             <ActionTooltip label="Delete" side="right">
               <Trash
-                onClick={() => onOpen("deleteChannel", { server, channel })}
+                onClick={(e) => onAction(e, "deleteChannel")}
                 className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 ddark:hover:text-zinc-300 transition"
               />
             </ActionTooltip>
