3a702a55b5bfefe1d87ec5dc344719c244b3919d
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-12-18 18:56:04 +0530

    Passed the props to ChatHeader Component with name, serverId, type

--- app/(All Servers)/(Server Routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(All Servers)/(Server Routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,50 @@
-const ChannelIdPage = () => {
+import ChatHeader from "@/components/chat/chat-header";
+import { Currentprofile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps{
+  params:{
+    serverId: string;
+    channelId: string;
+  }
+}
+
+const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
+  const profile = await Currentprofile();
+
+  if(!profile){
+    return redirectToSignIn();
+  }
+
+  // We are finding the channel by using the channelId
+  const channel = await db.channel.findUnique({
+    where:{
+      id: params.channelId,
+    },
+  });
+
+  // We are finding the user data. Since this "profile.id" can be a member of many other servers but we only need the which is in this server so we also fetch serverId from params
+  const member = await db.member.findFirst({
+    where:{
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+    // If any of this not present then return to home
+  if(!channel || !member){
+    return redirect(`/`);
+  }
+
   return (
-    <div>
-      This is Channel Id page
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+          name={channel.name}
+          serverId={channel.serverId}
+          type="channel"
+      />
     </div>
   )
 }
