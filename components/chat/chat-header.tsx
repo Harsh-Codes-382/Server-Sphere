bd16b36c634fa95a89431b956a2a87cc870e55d6
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-12-19 18:49:31 +0530

    Replaced the Menu Component with MobileToggle componenet and passed the serverId so we can pass that id to serverSideBar

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
-import { Hash, Menu } from "lucide-react";
+import { Hash} from "lucide-react";
+import { MobileToggle } from "@/components/mobile-toggle";
 
 interface ChatHeaderProps{
   serverId: string;
@@ -10,10 +11,13 @@ interface ChatHeaderProps{
 const ChatHeader = ({serverId, name, type, imageUrl}: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <Menu/>
-      {type === "channel" && (
+      <MobileToggle serverId={serverId}/>
+      {type === "channel" && ( 
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
       )}
+      <p className=" font-semibold text-md text-black dark:text-white">
+        {name}
+      </p>
     </div>
   )
 }
