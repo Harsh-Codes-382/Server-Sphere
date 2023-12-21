083024b35f2acc56db7c558936e93624a5d27a1a
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-12-21 19:14:13 +0530

    Added the imageUrl part so we on conversation type we can see the image of user

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,11 +1,12 @@
 import { Hash} from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
+import UserAvatar from "@/components/user-avatar";
 
 interface ChatHeaderProps{
   serverId: string;
   name: string;
   type: "channel" | "conversation";
-  imageUrl?: string;
+  imageUrl: string;
 }
 
 const ChatHeader = ({serverId, name, type, imageUrl}: ChatHeaderProps) => {
@@ -14,6 +15,12 @@ const ChatHeader = ({serverId, name, type, imageUrl}: ChatHeaderProps) => {
       <MobileToggle serverId={serverId}/>
       {type === "channel" && ( 
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
+      )}
+      {type === "conversation" && (
+        <UserAvatar 
+          src={imageUrl}
+          className="h-8 w-8 md:h-8 md:w-8 mr-2"
+        />
       )}
       <p className=" font-semibold text-md text-black dark:text-white">
         {name}
