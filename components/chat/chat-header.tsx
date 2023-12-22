acbf5c601c3fc3e259f47ca64a0d3806f8eebfaf
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-12-22 21:18:32 +0530

    Added the SocketIndicator component here so we can see the user is online or not

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash} from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
 import UserAvatar from "@/components/user-avatar";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 interface ChatHeaderProps{
   serverId: string;
@@ -25,6 +26,9 @@ const ChatHeader = ({serverId, name, type, imageUrl}: ChatHeaderProps) => {
       <p className=" font-semibold text-md text-black dark:text-white">
         {name}
       </p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator/>
+      </div>
     </div>
   )
 }
