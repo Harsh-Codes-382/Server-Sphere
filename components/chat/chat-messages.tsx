e04265417c7946c3bea5e770a1087802d2272747
Harsh-Codes-382 <harshkumar3826@gmail.com>
2024-01-02 19:43:16 +0530

    Rendering the data which is the fetched messages from dataBase

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { Member } from "@prisma/client";
+import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
+import { Fragment } from "react";
+
+//  Type of fetched Messages  
+type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile
+  }
+}
 
 interface ChatMessagesProps {
     name: string;
@@ -31,6 +39,7 @@ export const ChatMessages = ({
 
   const queryKey = `chat:${chatId}`;
   const {data, fetchNextPage, isFetchingNextPage, hasNextPage, status} = useChatQuery({ queryKey ,apiUrl, paramKey, paramValue});
+  console.log(data);
 
   if(status === 'loading'){
     return (
@@ -57,6 +66,18 @@ export const ChatMessages = ({
       <div className="flex flex-1 flex-col py-4 overflow-y-auto">
         <div className="flex-1"/>
         <ChatWelcome type={type} name={name}/>
+        <div className="flex flex-col-reverse mt-auto">
+          {data?.pages?.map((group, i) => (
+            <Fragment key={i}>
+              {group.items.map((message: MessageWithMemberWithProfile) => (
+                <div key={message.id}>
+                  {message.content}
+                </div>
+              ))}
+
+            </Fragment>
+          ))}
+        </div>
       </div>
     </>
   );
