16a99e0c776d9c0454f1e2aecb99a26ea859447f
Harsh-Codes-382 <harshkumar3826@gmail.com>
2024-01-03 18:52:35 +0530

    Created the interface to specify the Props types

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,8 +1,34 @@
-"use client"
-export const ChatItem = () =>{
-    return (
-        <div>
-            Hello
-        </div>
-    )
+"use client";
+
+import { Member, Profile } from "@prisma/client";
+
+interface ChatItemProps {
+  id: string;
+  content: string;
+  // Because member is a object which have a both info of member and profile coming from db
+  member: Member & {
+    profile: Profile;
+  };
+  timestamp: string;
+  fileUrl: string | null;
+  deleted: boolean;
+  currentMember: Member;
+  isUpdated: boolean;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
 }
+
+export const ChatItem = ({
+  id,
+  content,
+  member,
+  timestamp,
+  fileUrl,
+  deleted,
+  currentMember,
+  isUpdated,
+  socketQuery,
+  socketUrl,
+}: ChatItemProps) => {
+  return <div>Hello</div>;
+};
