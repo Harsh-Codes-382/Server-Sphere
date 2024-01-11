5aac92670904e48123da66996bd310d360c84636
Harsh-Codes-382 <harshkumar3826@gmail.com>
2024-01-11 19:54:12 +0530

    Just specified the query for PATCH and DELETE for message based on the request send here

--- pages/api/socket/messages/[messageId].ts
+++ pages/api/socket/messages/[messageId].ts
@@ -1,6 +1,7 @@
 import { CurrentprofilePages } from "@/lib/current-profile-pages";
 import { db } from "@/lib/db";
 import { NextApiResponseServerIo } from "@/types";
+import { MemberRole } from "@prisma/client";
 import { NextApiRequest } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponseServerIo){
@@ -61,7 +62,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponseS
       }
 
     // Find that particular message
-      const message = await db.message.findFirst({
+      let message = await db.message.findFirst({
         where:{
             id: messageId as string,
             channelId: channelId as string,
@@ -79,6 +80,66 @@ export default async function handler(req: NextApiRequest, res: NextApiResponseS
       if(!message || message.deleted){
         return res.status(404).json({error: "Message Not found"})
       }
+
+      const isMessageOwner = message.member.profileId === profile.id;
+      const isAdmin = member.role === MemberRole.ADMIN;
+      const isModerator = member.role === MemberRole.MODERATOR;
+      const Only_We_can_Modify_Message = isMessageOwner || isAdmin || isModerator;
+
+      if(!Only_We_can_Modify_Message){
+        return res.status(401).json({error: "You are not authorized to modify the message "})
+      }
+
+    //   If the request of deleting of message
+      if(req.method === "DELETE"){
+
+        message = await db.message.update({
+            where:{
+                id: messageId as string,
+            },
+            data:{
+                fileUrl: null,
+                content:"This message has been deleted",
+                deleted: true,
+            },
+            include:{
+                member:{
+                    include:{
+                        profile: true,
+                    }
+                }
+            }
+        });
+      }
+
+    //   If the request is of updating the message so, here we are only updating the content
+      if (req.method === "PATCH") {
+        if (!isMessageOwner){
+            return res.status(401).json({error: "Only Message Owner can update the message"})
+        }
+          message = await db.message.update({
+            where: {
+              id: messageId as string,
+            },
+            data: {
+              content,
+            },
+            include: {
+              member: {
+                include: {
+                  profile: true,
+                },
+              },
+            },
+          });
+      }
+
+      const  updateKey = `chat:${channelId}:messages:update`;
+
+      res?.socket?.server?.io.emit(updateKey, message);
+      return res.status(200).json(message);
+
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({error: "Internal Server Error"});
