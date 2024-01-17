b6a1231ee1938bd9e22b9eb051ce7bd2f862589d
Harsh-Codes-382 <harshkumar3826@gmail.com>
2024-01-17 19:09:53 +0530

    Now we created another useEffect() which is for when new message appear so, we have to scroll down to BottomRef and if user is far to above in message so,  we will not autoscroll to down but if scroll range is within 100 from bottom then we autoscroll to the view of user

--- hooks/use-chat-scroll.ts
+++ hooks/use-chat-scroll.ts
@@ -28,4 +28,36 @@ export const useChatScroll = ({chatRef, bottomRef, shouldLoadMore, loadMore, cou
         return ()=> topDiv?.removeEventListener("scroll", handleScroll);
     },[shouldLoadMore, chatRef, loadMore]);
 
+
+    // This useEffect is for when new message appear so, our 'div' scroll to the bottom to show the new arrived message
+    useEffect(()=>{
+        const bottomDiv = bottomRef?.current;
+        const topDiv = chatRef?.current;
+        const shouldAutoScroll = () =>{
+            if(!hasInitialized && bottomDiv){
+                setHasInitialized(true);
+                return true;
+            }
+            if(!topDiv){
+                return false;
+            }
+
+            // Lets calculate the distance of user scroll to the bottom if the user is too far above then we don't auto scroll but 
+            // otherwise we do autoscroll to bottom
+
+            const distanceFromBottom = topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
+            return distanceFromBottom <= 100;
+        }
+
+        // If user is not far from bottom means under 100 then scroll this bottomRef to user view
+        if(shouldAutoScroll()){
+            setTimeout(()=>{
+                bottomRef?.current?.scrollIntoView({
+                    behavior: "smooth",
+                })
+            },100);
+        }
+ 
+    }, [bottomRef, chatRef, count, hasInitialized]); 
+
 }
