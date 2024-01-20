a100609837f025c878885bb06c763cdec213bfda
Harsh-Codes-382 <harshkumar3826@gmail.com>
2024-01-20 21:08:13 +0530

    Conditionally rendering the message and Input component only when channel type is not Video or Audio but if they are Audio channel then render the MediaRoom with video=false, audio=true  and  If channel is video type then MedaiRoom component with video=true, audio=true and channelId is act as a roomId so, all channel Member people can access this

--- app/(All Servers)/(Server Routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(All Servers)/(Server Routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,15 @@
 import ChatHeader from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
 import { ChatMessages } from "@/components/chat/chat-messages";
+import { MediaRoom } from "@/components/media-room";
+
 import { Currentprofile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+
 import { redirectToSignIn } from "@clerk/nextjs";
+
+import { ChannelType } from "@prisma/client";
+
 import { redirect } from "next/navigation";
 
 interface ChannelIdPageProps{
@@ -43,37 +49,54 @@ const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader
-          name={channel.name}
-          serverId={channel.serverId}
-          type="channel"
-      />
-      <ChatMessages
         name={channel.name}
-        member={member}
-        chatId={channel.id}
+        serverId={channel.serverId}
         type="channel"
-        // This apiUrl will be url of fetching the messages
-        apiUrl="/api/messages"
-        socketUrl="/api/socket/messages"
-        socketQuery={{
-          channelId: channel.id,
-          serverId: channel.serverId,
-        }}
-        paramKey="channelId"
-        paramValue={channel.id}
       />
+
+      {/* So, this messages and input for messages only available for channel Type TEXT not for Video, Audio */}
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <ChatMessages
+            name={channel.name}
+            member={member}
+            chatId={channel.id}
+            type="channel"
+            // This apiUrl will be url of fetching the messages
+            apiUrl="/api/messages"
+            socketUrl="/api/socket/messages"
+            socketQuery={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+            paramKey="channelId"
+            paramValue={channel.id}
+          />
+
+          <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+          />
+        </>
+      )}
+
+      {/* So ,when channelType is AUDIO then render this component which was written from LiveKit docs and passed the video as false */}
+      {channel.type === ChannelType.AUDIO && (
+        <MediaRoom video={false} audio={true} chatId={channel.id} />
+      )}
       
-      <ChatInput
-        name={channel.name}
-        type="channel"
-        apiUrl="/api/socket/messages"
-        query={{
-          channelId:channel.id,
-          serverId: channel.serverId,
-        }}
-        />
+      {/* So ,when channelType is VIDEO then render this component which was written from LiveKit docs and passed both the video & audio as true */}
+
+      {channel.type === ChannelType.VIDEO && (
+        <MediaRoom video={true} audio={true} chatId={channel.id} />
+      )}
     </div>
-  )
+  );
 }
 
 export default ChannelIdPage
